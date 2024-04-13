f0d18e06f428cae28b47ea3b833993af2725c6fe
gustavo-fardo <gustavo.fardo@hotmail.com>
2024-04-13 18:38:32 -0300

    mux ready, flag zero too

--- ULA/ULA.vhd
+++ ULA/ULA.vhd
@@ -4,6 +4,7 @@ use ieee.numeric_std.all;
 
 entity ULA is
     port(
+        sel   : in unsigned(1 downto 0);
         ent_a : in unsigned(15 downto 0);
         ent_b : in unsigned(15 downto 0);
         saida : out unsigned(15 downto 0);
@@ -23,21 +24,37 @@ architecture a_ULA of ULA is
         );
     end component;
 
-    -- component mux
-    --     port(
-    --         sel   : in  unsigned(1 downto 0);
-    --         ent0  : in  unsigned(15 downto 0);  --adder
-    --         ent1  : in  unsigned(15 downto 0); 
-    --         ent2  : in  unsigned(15 downto 0); 
-    --         ent3  : in  unsigned(15 downto 0);
-    --         saida : out unsigned(15 downto 0)
-    --     );
-    -- end component;
+    component mux4x1_16bits
+        port(
+            sel   : in  unsigned(1 downto 0);
+            ent0  : in  unsigned(15 downto 0);  --adder
+            ent1  : in  unsigned(15 downto 0); 
+            ent2  : in  unsigned(15 downto 0); 
+            ent3  : in  unsigned(15 downto 0);
+            saida : out unsigned(15 downto 0)
+        );
+    end component;
+
+    signal saida_add, saida_sub, saida_op3, saida_op4, saida_mux : unsigned(15 downto 0);
 
 begin
-    adder : adder_16bits port map(ent_a=>ent_a,
-                                    ent_b=>ent_b,
-                                    car_in=>'0',
-                                    saida=>saida,
-                                    car_out=>carry);
+    adder : adder_16bits port map(  ent_a => ent_a,
+                                    ent_b => ent_b,
+                                    car_in => '0',
+                                    saida => saida_add,
+                                    car_out => carry);
+
+    mux : mux4x1_16bits port map(   sel => sel,
+                                    ent0 => saida_add,
+                                    ent1 => saida_sub,
+                                    ent2 => saida_op3,
+                                    ent3 => saida_op4,
+                                    saida => saida_mux);
+
+    saida <= saida_mux;
+    zero <= not(saida_mux(0) or saida_mux(1) or saida_mux(2) or saida_mux(3) 
+                or saida_mux(4) or saida_mux(5) or saida_mux(6) or saida_mux(7) 
+                or saida_mux(8) or saida_mux(9) or saida_mux(10) or saida_mux(11) 
+                or saida_mux(12) or saida_mux(13) or saida_mux(14) or saida_mux(15));
+
 end architecture;
--- ULA/ULA_tb.vhd
+++ ULA/ULA_tb.vhd
@@ -8,6 +8,7 @@ end entity;
 architecture a_ULA_tb of ULA_tb is
     component ULA
         port(
+            sel   : in unsigned(1 downto 0);
             ent_a : in unsigned(15 downto 0);
             ent_b : in unsigned(15 downto 0);
             saida : out unsigned(15 downto 0);
@@ -16,13 +17,15 @@ architecture a_ULA_tb of ULA_tb is
         );
     end component;
     --definicao dos sinais
+    signal sel : unsigned(1 downto 0);
     signal ent_a, ent_b, saida : unsigned(15 downto 0);
     signal zero, carry: std_logic;
 
     begin
         --instancia do component porta
         -- map(pino => sinal)
-        uut: ULA port map(ent_a => ent_a,
+        uut: ULA port map(sel => sel,
+                          ent_a => ent_a,
                           ent_b => ent_b,
                           saida => saida,
                           zero => zero,
@@ -30,6 +33,7 @@ architecture a_ULA_tb of ULA_tb is
                             
         process
         begin
+            sel <= "01";
             ent_a <= "0000000000000000";
             ent_b <= "0000000000000001";
             wait for 50 ns;
